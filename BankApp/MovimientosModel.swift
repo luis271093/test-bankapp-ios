1066e706eaf22eccb81f87e3358a1c6f1f49d759
luis hector hernandez godinez <roko789-@hotmail.com>
2022-05-19 23:32:14 -0500

    actualizacion de servicios

---
+++ BankApp/MovimientosModel.swift
@@ -0,0 +1,8 @@
+//
+//  MovimientosModel.swift
+//  BankApp
+//
+//  Created by luis hector hernandez godinez  on 19/05/22.
+//
+
+import Foundation
---
+++ BankApp/MovimientosTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovimientosTableViewCell.swift
+//  BankApp
+//
+//  Created by luis hector hernandez godinez  on 19/05/22.
+//
+
+import UIKit
+
+class MovimientosTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
